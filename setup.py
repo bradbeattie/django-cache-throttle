e126b6acbad92e83924cba41be3e507b4606605d
Brad Beattie <brad@gossamer-threads.com>
2012-11-09 11:17:57 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-cache-throttle',
+    version="2012.11.09.0",
+    description='Cache-based rate-limiting for Django.',
+    long_description=open('README.md').read(),
+    author='Brad Beattie',
+    author_email='bradbeattie@gmail.com',
+    url='https://github.com/bradbeattie/django-cache-throttle',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data = { '': ['README.md'] },
+    install_requires=['django'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
